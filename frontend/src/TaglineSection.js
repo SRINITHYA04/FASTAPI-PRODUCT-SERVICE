266bc61c8149d2c19583ae2c915f7b8361793930
SRINITHYA04 <sn4812648@gmail.com>
2026-05-02 13:51:33 +0530

    feat: Initial setup with frontend

---
+++ frontend/src/TaglineSection.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TaglineSection.css";
+
+const TaglineSection = () => {
+  return (
+    <div className="tagline-card">
+      <div className="tagline-content">
+        <h3>📈 Track. Manage. Grow.</h3>
+        <p>
+          Streamline your inventory with smart product management that scales
+          with your business.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaglineSection;
